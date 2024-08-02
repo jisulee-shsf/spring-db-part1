c28821b0676172ce51fe67fdb9e5b9a573c5c863
Jisu Lee <jisulee.shsf@gmail.com>
2024-08-02 15:59:53 +0900

    Initial commit

---
+++ src/main/java/study/springdbpart1/SpringDbPart1Application.java
@@ -0,0 +1,13 @@
+package study.springdbpart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbPart1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbPart1Application.class, args);
+    }
+
+}
---
+++ src/test/java/study/springdbpart1/SpringDbPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springdbpart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbPart1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
